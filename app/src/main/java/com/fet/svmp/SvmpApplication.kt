7e6ed16385870db331421bff43a4b9ecc070eeb2
Yuan Chen <jackie61109@gmail.com>
2018-12-04 11:20:47 +0800

    + Log - third party com.orhanobut;

--- app/src/main/java/com/fet/svmp/SvmpApplication.kt
+++ app/src/main/java/com/fet/svmp/SvmpApplication.kt
@@ -2,14 +2,39 @@ package com.fet.svmp
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.fet.svmp.customize.Configs
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import io.fabric.sdk.android.Fabric
 
 /**
  * Created by i_jackiechen1 on 2018/12/3 下午2:59.
  */
 class SvmpApplication : Application() {
+
+    val LOG_TAG = "SVMP"
+
     override fun onCreate() {
         super.onCreate()
         Fabric.with(this, Crashlytics())
+        logInitial()
+    }
+
+    private fun logInitial() {
+        Logger.addLogAdapter(object : AndroidLogAdapter() {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return Configs.IS_DEBUG
+            }
+        })
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+//                .showThreadInfo(false)  //是否选择显示线程信息，默认为true
+//                .methodCount(0)         //方法数显示多少行，默认2行
+//                .methodOffset(7)        //隐藏方法内部调用到偏移量，默认5
+//                .logStrategy(customLog) //打印日志的策略，默认LogCat
+                .tag(LOG_TAG)   //自定义TAG全部标签，默认PRETTY_LOGGER
+                .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
     }
 }
---
+++ app/src/main/java/com/fet/svmp/customize/Configs.kt
@@ -0,0 +1,11 @@
+package com.fet.svmp.customize
+
+import com.fet.svmp.BuildConfig
+
+/**
+ * Created by i_jackiechen1 on 2018/12/4 上午10:42.
+ */
+object Configs {
+
+    val IS_DEBUG = BuildConfig.IS_DEBUG
+}
